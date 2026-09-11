212aa012bc12de1b22ec491920011d2b50b38baa
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-09-12 01:53:41 +0700

    style(dashboard): switch to Noto Sans Thai variable font

--- apps/dashboard/lib/fonts.ts
+++ apps/dashboard/lib/fonts.ts
@@ -1,15 +1,15 @@
 import {
   Geist_Mono as createMono,
-  IBM_Plex_Sans_Thai as createSans,
+  Noto_Sans_Thai as createSans,
 } from "next/font/google";
 
 import { cn } from "./utils";
 
 const sans = createSans({
   display: "swap",
-  subsets: ["thai", "latin"],
+  subsets: ["latin", "thai"],
   variable: "--font-sans",
-  weight: ["300", "400", "500", "600", "700"],
+  weight: "variable",
 });
 
 const mono = createMono({
